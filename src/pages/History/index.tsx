a113ea00e4007dae42fe15256fc7692d2e032573
basya01 <arsen.lulukyan@gmail.com>
2022-09-28 23:29:12 +0300

    fix render avarage

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -4,22 +4,24 @@ import styles from './History.module.scss';
 
 const History = () => {
   const history = useAppSelector((state) => state.history);
-  
+
   const avarage = history.reduce((sum, item) => sum + item.result, 0) / history.length;
 
   return (
     <>
       <p className={styles.title}>History</p>
-      <p className={styles.avarage}>Avarage: {Math.round(avarage)}%</p>
       {history.length ? (
-        <ul>
-          {history.map((item) => (
-            <li key={item.date} className={styles.item}>
-              <p>{item.date}</p>
-              <p>{Math.round(item.result)}%</p>
-            </li>
-          ))}
-        </ul>
+        <>
+          <p className={styles.avarage}>Avarage: {Math.round(avarage)}%</p>
+          <ul>
+            {history.map((item) => (
+              <li key={item.date} className={styles.item}>
+                <p>{item.date}</p>
+                <p>{Math.round(item.result)}%</p>
+              </li>
+            ))}
+          </ul>
+        </>
       ) : (
         <p>History is empty</p>
       )}
