5d6a7f0b48ab53572dd105935a226c1393da978d
basya01 <arsen.lulukyan@gmail.com>
2022-09-29 12:21:49 +0300

    refactor getTime

--- src/utils/getTime.ts
+++ src/utils/getTime.ts
@@ -1,13 +1,9 @@
 export const getTime = (date: Date) => {
-  return (
-    date.getDate().toString().padStart(2, '0') +
-    '.' +
-    (date.getMonth() + 1).toString().padStart(2, '0') +
-    '.' +
-    date.getFullYear() +
-    ' ' +
-    date.getHours().toString().padStart(2, '0') +
-    ':' +
-    date.getMinutes().toString().padStart(2, '0')
-  );
+  return `
+    ${date.getDate().toString().padStart(2, '0')}.
+    ${(date.getMonth() + 1).toString().padStart(2, '0')}.
+    ${date.getFullYear()} 
+    ${date.getHours().toString().padStart(2, '0')}:
+    ${date.getMinutes().toString().padStart(2, '0')}
+  `;
 };
